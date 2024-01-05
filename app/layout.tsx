224039241ee6b61a43da53742bd9075e1c79e455
Redskull-127 <meerraja17@gmail.com>
2024-01-05 23:28:16 +0530

    Add favicon to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import NextAuthProvider from "@/lib/client/providers/NextAuthSessionProvider";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Children } from "@/lib/types/children";
 import OG_IMG from "@/public/static/opengraph/image.png";
+import Favicon from "@/public/favicon.ico";
 
 const santoshiSans = localFont({
   src: "./Satoshi-Variable.woff2",
@@ -21,15 +22,17 @@ const santoshiSans = localFont({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.meertarbani.in"),
   title: `Meer Tarbani`,
   description: `Meer Tarbani's Portfolio Website`,
   keywords:
     "Meer Tarbani, Meer, Tarbani, Portfolio, Website, meer tarbani, meer tarbani acid, meer tarbani as a fraction, meer tarbani ba, meer tarbani bali, meer tarbani bangla, meer tarbani bangla lyrics, meer tarbani battery, meer tarbani bank, meer tarbani blood pressure, meer tarbani chords, meer tarbani com, meer tarbani code, meer tarbani center, meer tarbani de, meer tarbani delhi, meer tarbani dei, meer tarbani dit, meer tarbani definition, meer tarbani download, meer tarbani disease, meer tarbani english translation, meer tarbani english lyrics, meer tarbani english, meer tarbani english pdf, meer tarbani english subtitles, meer tarbani englisch, meer tarbani example, meer tarbani film, meer tarbani facebook, meer tarbani full movie, meer tarbani fakaza, meer tarbani font, meer tarbani file, meer tarbani for sale,  eer tarbani gif, meer tarbani google translate,  meer tarbani google scholar, meer tarbani google, meer tarbani google drive, meer tarbani games, meer tarbani germany",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
-  },
+  icons: [
+    {
+      rel: "icon",
+      url: Favicon.src,
+    },
+  ],
   manifest: "/manifest.json",
   openGraph: {
     title: "Meer Tarbani",
