0b0af3669ba64423ee63f2a2bb03e741a24d549f
Redskull-127 <meerraja17@gmail.com>
2024-01-20 19:01:06 +0530

    Refactor Spotify API functions and add error handling

--- lib/server/functions/spotify.ts
+++ lib/server/functions/spotify.ts
@@ -13,6 +13,76 @@ export type SpotifyType = {
   uri?: string;
 };
 
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_SECRET_ID = process.env.SPOTIFY_SECRET_ID;
+const SPOTIFY_REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN;
+const NOW_PLAYING_URL = "https://api.spotify.com/v1/me/player/currently-playing";
+const RECENTLY_PLAYING_URL = `https://api.spotify.com/v1/playlists/42h3IewUsTfRNHE5Puw9EK/tracks?limit=50&offset=${Math.floor(
+  Math.random() * 951
+)}`;
+const REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+let SPOTIFY_TOKEN = "";
+
+const getAuthHeader = () => {
+  const authString = `${SPOTIFY_CLIENT_ID}:${SPOTIFY_SECRET_ID}`;
+  return Buffer.from(authString).toString("base64");
+};
+
+const requestRefreshToken = async () => {
+  const data = new URLSearchParams({
+    grant_type: "refresh_token",
+    refresh_token: SPOTIFY_REFRESH_TOKEN as string,
+  });
+
+  const headers = {
+    Authorization: `Basic ${getAuthHeader()}`,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  try {
+    const response = await fetch(REFRESH_TOKEN_URL, {
+      method: "POST",
+      headers,
+      next: {
+        tags: ["spotifyAPI"],
+        revalidate: 1,
+      },
+      body: data,
+    });
+    const resp = await response.json();
+    return resp.access_token;
+  } catch (error) {
+    // Handle error
+  }
+};
+
+const makeRequest = async (url: string, config = {}): Promise<any> => {
+  if (!SPOTIFY_TOKEN) {
+    SPOTIFY_TOKEN = await requestRefreshToken();
+  }
+
+  try {
+    const makeResponse = await fetch(url, {
+      // ...config,
+      headers: {
+        Authorization: `Bearer ${SPOTIFY_TOKEN}`,
+      },
+      next: {
+        tags: ["spotifyAPI"],
+        revalidate: 1,
+      },
+    });
+    const res = await makeResponse.json();
+
+    return res;
+  } catch (error: any) {
+    if (error.response?.status === 401) {
+      SPOTIFY_TOKEN = await requestRefreshToken();
+      return makeRequest(url);
+    }
+  }
+};
 
 export async function getNewSong() {
   return revalidateTag("spotifyAPI");
@@ -20,77 +90,6 @@ export async function getNewSong() {
 
 export const SpotifySelfApi = unstable_cache(
   async () => {
-    const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-    const SPOTIFY_SECRET_ID = process.env.SPOTIFY_SECRET_ID;
-    const SPOTIFY_REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN;
-    const NOW_PLAYING_URL =
-      "https://api.spotify.com/v1/me/player/currently-playing";
-    const RECENTLY_PLAYING_URL = `https://api.spotify.com/v1/playlists/42h3IewUsTfRNHE5Puw9EK/tracks?limit=50&offset=${Math.floor(
-      Math.random() * 951
-    )}`;
-    const REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token";
-    let SPOTIFY_TOKEN = "";
-
-    const getAuthHeader = () => {
-      const authString = `${SPOTIFY_CLIENT_ID}:${SPOTIFY_SECRET_ID}`;
-      return Buffer.from(authString).toString("base64");
-    };
-
-   
-
-    const requestRefreshToken = async () => {
-      const data = new URLSearchParams({
-        grant_type: "refresh_token",
-        refresh_token: SPOTIFY_REFRESH_TOKEN as string,
-      });
-
-      const headers = {
-        Authorization: `Basic ${getAuthHeader()}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      };
-
-      try {
-        const response = await fetch(REFRESH_TOKEN_URL, {
-          method: "POST",
-          headers,
-          next: {
-            tags: ["spotifyAPI"],
-            revalidate: 1
-          },
-          body: data,
-        });
-        const resp = await response.json();
-        return resp.access_token;
-      } catch (error) {
-      }
-    };
-
-    const makeRequest = async (url: string, config = {}): Promise<any> => {
-      if (!SPOTIFY_TOKEN) {
-        SPOTIFY_TOKEN = await requestRefreshToken();
-      }
-
-      try {
-        const makeResponse = await fetch(url, {
-          // ...config,
-          headers: {
-            Authorization: `Bearer ${SPOTIFY_TOKEN}`,
-          },
-          next: {
-            tags: ["spotifyAPI"],
-            revalidate: 1
-          },
-        });
-        const res = await makeResponse.json();
-        
-        return res;
-      } catch (error: any) {
-        if (error.response?.status === 401) {
-          SPOTIFY_TOKEN = await requestRefreshToken();
-          return makeRequest(url);
-        }
-      }
-    };
     try {
       const data = await makeRequest(NOW_PLAYING_URL);
       if (data.item) {
@@ -116,13 +115,13 @@ export const SpotifySelfApi = unstable_cache(
           } as SpotifyType;
         }
       } catch (innerError) {
-        return;
+        // Handle inner error
       }
     }
   },
   ["spotifyAPI"],
   {
     tags: ["spotifyAPI"],
-    revalidate: 1
+    revalidate: 1,
   }
 );
