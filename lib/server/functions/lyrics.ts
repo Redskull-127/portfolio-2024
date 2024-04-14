e9d1266564ac68de025753b74ca4ebdf3794f515
Redskull-127 <meerraja17@gmail.com>
2024-04-14 17:17:03 +0530

    Update lyrics.ts and next.config.js

--- lib/server/functions/lyrics.ts
+++ lib/server/functions/lyrics.ts
@@ -1,7 +1,7 @@
-import { Client } from "genius-lyrics"
+import Lyrics from "genius-lyrics"
 
 export default async function getLyrics(song: string) {
-  const LyricsClient = new Client()
+  const LyricsClient = new Lyrics.Client()
   const songs = await LyricsClient.songs.search(song)
   const data = songs[0]
   return data as typeof data
--- next.config.js
+++ next.config.js
@@ -1,32 +1,52 @@
 // @ts-check
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.scdn.co',
-                pathname: '/image/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '/**',
-            }
-        ]
+  reactStrictMode: false,
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.tsx',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ]
     }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        pathname: '/image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      }
+    ]
+  }
 }
 
 const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    disable: process.env.NODE_ENV === 'development' ? true : false,
-    workboxOptions: {
-        disableDevLogs: true,
-    }
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === 'development' ? true : false,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
 });
 
 module.exports = withPWA(nextConfig);
