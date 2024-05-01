cf6a960fada9fea6520161a477cea3f0ce99c55b
Redskull-127 <meerraja17@gmail.com>
2024-05-01 15:51:30 +0530

    Update 3D-avatar.tsx to add userSelect style property

--- components/Root-Partials/3D-avatar.tsx
+++ components/Root-Partials/3D-avatar.tsx
@@ -3,11 +3,12 @@ import Spline from "@splinetool/react-spline";
 
 export default function Avatar3D() {
   return (
-    <div className="absolute size-[400px] object-fill overflow-hidden">
+    <div className="absolute size-[400px] object-fill overflow-hidden select-none">
       <Spline
         style={{
           position: "absolute",
           animation: "fade-in 1s ease-in-out",
+          userSelect: "none",
         }}
         scene="https://prod.spline.design/47G-sHHwpZlB68OX/scene.splinecode"
       />
