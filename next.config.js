28aca8e12bbf70c4cc2133ca5ce8a7990ed4e1d4
Redskull-127 <meerraja17@gmail.com>
2024-07-01 00:28:05 +0530

    chore: Add redirects for social media links

--- next.config.js
+++ next.config.js
@@ -16,6 +16,40 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://www.github.com/redskull-127',
+        permanent: true,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/meertarbani/',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://www.twitter.com/meertarbani',
+        permanent: true,
+      },
+      {
+        source: '/spotify',
+        destination: 'https://open.spotify.com/user/to6rms2g0fzerpkwox1k4v33w',
+        permanent: true,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.com/users/redskull127',
+        permanent: true,
+      },
+      {
+        source: '/source-code',
+        destination: 'https://github.com/redskull-127/Portfolio-2024',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withPWA = require('@ducanh2912/next-pwa').default({
