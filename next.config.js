abeafb17ac911824716d7906571052355fc94828
Redskull-127 <meerraja17@gmail.com>
2024-07-01 00:45:30 +0530

    chore: Update social media link redirects and remove basePath configuration

--- next.config.js
+++ next.config.js
@@ -22,31 +22,37 @@ const nextConfig = {
         source: '/github',
         destination: 'https://www.github.com/redskull-127',
         permanent: true,
+        basePath: false,
       },
       {
         source: '/linkedin',
         destination: 'https://www.linkedin.com/in/meertarbani/',
         permanent: true,
+        basePath: false,
       },
       {
         source: '/twitter',
         destination: 'https://www.twitter.com/meertarbani',
         permanent: true,
+        basePath: false,
       },
       {
         source: '/spotify',
         destination: 'https://open.spotify.com/user/to6rms2g0fzerpkwox1k4v33w',
         permanent: true,
+        basePath: false,
       },
       {
         source: '/discord',
         destination: 'https://discord.com/users/redskull127',
         permanent: true,
+        basePath: false,
       },
       {
         source: '/source-code',
         destination: 'https://github.com/redskull-127/Portfolio-2024',
         permanent: true,
+        basePath: false,
       },
     ];
   },
