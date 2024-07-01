33d215d97566e405f1563746db4369f7f44b0d48
Redskull-127 <meerraja17@gmail.com>
2024-07-01 15:57:17 +0530

    feat: refactored intro dialog to not show while 404 page error!

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -1,15 +1,17 @@
 'use client';
 import { useState, useEffect } from 'react';
 import IntroDialog from './intro-dialog';
+import { usePathname } from 'next/navigation';
 export default function IntroProvider() {
+  const path = usePathname();
   const [intro, setIntro] = useState(false);
 
   useEffect(() => {
     const intro = localStorage.getItem('intro-visited');
-    if (!intro || intro === 'false') {
+    if ((!intro || intro === 'false') && path === '/') {
       setIntro(true);
     }
-  }, []);
+  }, [path]);
 
   if (!intro) return null;
   if (intro) {
