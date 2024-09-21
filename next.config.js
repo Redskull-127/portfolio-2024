2622002ac5b434278d4be6797f65c048bfb11c81
Restyled.io <commits@restyled.io>
2024-09-21 22:11:06 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -1,39 +1,41 @@
-const createNextIntlPlugin = require('next-intl/plugin');
+const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin(
-    './lib/internationalization/request.ts',
+  "./lib/internationalization/request.ts",
 );
 
 /** @type {import('next').NextConfig} */
-const config = require('./config');
+const config = require("./config");
 
 const nextConfig = withNextIntl({
-  reactStrictMode : false,
-  basePath : '',
-  images : {
-    remotePatterns : config.remotePatterns,
+  reactStrictMode: false,
+  basePath: "",
+  images: {
+    remotePatterns: config.remotePatterns,
   },
-  async redirects() { return config.redirects; },
-  webpack : (config, {webpack}) => {
+  async redirects() {
+    return config.redirects;
+  },
+  webpack: (config, { webpack }) => {
     config.plugins.push(
-        new webpack.IgnorePlugin({
-          resourceRegExp : /^pg-native$|^cloudflare:sockets$/,
-        }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      }),
     );
 
     return config;
   },
 });
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest : 'public',
-  cacheOnFrontEndNav : true,
-  aggressiveFrontEndNavCaching : true,
-  reloadOnOnline : true,
-  disable : process.env.NODE_ENV === 'development',
-  workboxOptions : {
-    disableDevLogs : process.env.NODE_ENV === 'development' ? false : true,
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: process.env.NODE_ENV === "development" ? false : true,
   },
 });
 
 module.exports =
-    process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig);
+  process.env.NODE_ENV === "development" ? nextConfig : withPWA(nextConfig);
